6f697d071f54ba992c23a2e84be4a6e97c546e7b
KraftyKoder <krrishk567@gmail.com>
2023-04-22 22:13:37 -0700

    Co-authored-by: kalkulator413 <kalkulator413@users.noreply.github.com> Co-authored-by: Manan Patel <mananpatel3674@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const redis = require('redis');
+const lib = require("./speechToText");
 
 const redisClient = redis.createClient();
 redisClient.on('error', err => console.log('Redis Client Error', err));
@@ -7,6 +8,42 @@ redisClient.on('error', err => console.log('Redis Client Error', err));
 const accountSid = process.env.TWILIO_ACCT_SID;
 const authToken = process.env.TWILIO_AUTH;
 const client = require('twilio')(accountSid, authToken);
+
+const express = require('express');
+const { sentiment } = require('sentiment.js');
+
+const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+const SpeechRecognitionEvent = window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+const audioRecognition = new SpeechRecognition();
+audioRecognition.continuous = true;
+audioRecognition.lang = "en-US";
+audioRecognition.interimResults = true;
+audioRecognition.maxAlternatives = 1;
+audioRecognition.onstart = function() {
+    // console.log("Microphone recording started");
+}
+
+audioRecognition.onresult = function(event) {
+    var script = event.results[event.resultIndex][0].transcript;
+    // console.log(script);
+    document.getElementById("transcript").innerHTML = script;
+}
+
+audioRecognition.onend = function()
+{
+    audioRecognition.abort();
+    // console.log("Microphone recording ended");
+    audioRecognition.start();
+}
+
+module.exports = {
+    setupAudio,
+    audioRecognition
+}
+
+const app = express();
+app.use(express.json());
 
 const sendMessage = async (message, toPhoneNumber) => {
     await redisClient.connect();
@@ -38,4 +75,26 @@ const sendMessage = async (message, toPhoneNumber) => {
         );
 };
 
-sendMessage("hi", "+15107661520")
+
+
+// Endpoint to fetch data
+app.get('/', async (req, res) => {
+  try {
+    const data = await sentiment(res.query.speech);
+    if(data['body']['classifications'][0]['prediction'] == 'Crime'){
+        res.send("Threat Detected");
+    } else {
+        res.send("No Threat Detected");
+    }
+    res.status(200).json({ data });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('Server listening on port ' + process.env.PORT);
+});
+// sendMessage("hi", "+15107661520")
+// setupAudio();
+// audioRecognition.start();
---
+++ backend/speechToText.js
@@ -0,0 +1,7 @@
+// function setupAudio() {
+
+// }
+
+
+
+// audioRecognition.start();
